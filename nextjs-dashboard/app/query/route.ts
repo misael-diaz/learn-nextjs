ef21ab125f5332239c8b36d103f621e91359a043
Misael Diaz Maldonado <20134959+misael-diaz@users.noreply.github.com>
2025-08-23 20:53:25 +0000

    refactors query route
    NOTES
    modifies the source code so that it uses `pg` for querying the database

--- nextjs-dashboard/app/query/route.ts
+++ nextjs-dashboard/app/query/route.ts
@@ -1,26 +1,30 @@
-// import postgres from 'postgres';
+const { Pool } = require("pg");
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+async function listInvoices(connectionPool: Pool) {
+  const data = await connectionPool.query(`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `);
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
-
-// 	return data;
-// }
+  return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+
+  const connectionPool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
   });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+
+  try {
+    return Response.json(await listInvoices(connectionPool));
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
